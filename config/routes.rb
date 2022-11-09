a5c742cec7045978c27a907946ed761a7a00b754
Kayracz <kayracz@gmail.com>
2022-11-08 21:16:46 -0400

    added all pages and there routes tied to new designed navbar, also advanced in the design of the homepage and css

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root to: "articles#homepage"
   get "store" => "articles#index"
   get "about" => "articles#about"
+  get "contact" => "articles#contact"
+  get "price" => "articles#price"
+  get "classes" => "articles#classes"
   resources :articles, except: [:index,]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
